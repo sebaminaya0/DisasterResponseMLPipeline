30f4a165570ac530155200c303cd419d3ac98cfb
Sebastián Alonso Minaya Alva <sebastian.minaya@jokr.it>
2021-11-24 09:39:13 -0500

    Solved web_app error

---
+++ web_app v2/app/data/process_data.py
@@ -0,0 +1,100 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    """Extracts data from the messages and categories files, then merge both to work with a unique DataFrame.
+
+    Args:
+        messages_filepath (str): file path of the messages file.
+        categories_filepath (str): file path of the categories file.
+
+    Returns:
+        df (pandas.DataFrame): result DataFrame with the extracted information of messages and categories.
+    """
+
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    df = messages.merge(categories,left_on=['id'],right_on=['id'])
+
+    return df
+
+
+def clean_data(df):
+    """Transforms DataFrame with all necessary steps.
+
+    Args:
+        df (pandas.DataFrame): DataFrame that needs cleaning.
+
+    Returns:
+        df (pandas.DataFrame): DataFrame already cleaned.
+    """
+
+    categories = df['categories'].str.split(';',expand=True)
+
+    category_colnames = [x[0:-2] for x in list(categories.iloc[0,:])]
+
+    categories.columns = category_colnames
+
+    categories.replace('related-2','related-1',inplace=True)
+
+    for column in categories:
+        categories[column] = categories[column].astype(str).str.split("-").str.get(1)
+
+        categories[column] = pd.to_numeric(categories[column])
+
+    df.drop('categories', axis=1, inplace=True)
+
+    df = pd.concat([df, categories], axis=1)
+
+    df = df.drop_duplicates()
+
+    return df
+
+
+def save_data(df, database_filename):
+    """Loads data into a SQLite database specified by the user.
+
+    Args:
+        df (pandas.DataFrame): DataFrame already cleaned.
+        database_filename (str): filepath of the database.
+
+    Returns:
+        None
+    """
+
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('fact_messages', engine, index=False, if_exists='replace')
+
+    return None
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+
+        print('Cleaned data saved to database!')
+
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ web_app v2/app/run.py
@@ -0,0 +1,144 @@
+import json
+import plotly
+import pandas as pd
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar, Pie, Box
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('fact_messages', engine)
+
+# load model
+model = joblib.load("../models/classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+
+    # extract data needed for visuals
+
+    # Graph 1 Data
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+
+    # Graph 2 Data
+    df_ = pd.melt(df.drop(['message','original','genre'], axis=1),['id'])
+
+    categories_sum = df_.groupby('variable')['value'].sum().sort_values(ascending=False)
+    categories_names = list(categories_sum.index)
+
+    # Graph 3 Data
+    msg_length = df
+
+    msg_length['msg_length'] = msg_length['message'].str.len()
+
+    msg_length = df[['genre','msg_length']]
+
+    max_direct = msg_length[msg_length['genre']=='direct']['msg_length'].max()
+    msg_length = msg_length[msg_length['msg_length'] <= max_direct]
+
+    # create visuals
+
+    graphs = [
+        # Graph 1
+        {
+            'data': [
+                Pie(
+                    labels=genre_names,
+                    values=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres'
+            }
+        },
+        # Graph 2
+        {
+            'data': [
+                Bar(
+                    x=categories_names,
+                    y=categories_sum
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count of Messages"
+                },
+                'xaxis': {
+                    'title': "Categories"
+                }
+            }
+        },
+        # Graph 3
+        {
+            'data': [
+                Box(
+                    x=msg_length['genre'],
+                    y=msg_length['msg_length']
+                )
+            ],
+
+            'layout': {
+                'title': 'Message Length by Genre - Limited by Max Length of Direct Messages'
+            }
+        }
+    ]
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '')
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file.
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
